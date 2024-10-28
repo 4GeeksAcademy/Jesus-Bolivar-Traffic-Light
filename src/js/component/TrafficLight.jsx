67bf471eed112d82eec3d7ef8738315e03584c7d
yesus011 <jesu8907@gmail.com>
2024-10-28 19:52:32 +0000

    Semaforo-1ero

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,31 @@
+import React, { useState }from "react";
+
+
+const TrafficLigt = () => {
+
+    const[on, setOn]=useState("red")
+
+    const changeColor = (color) =>{
+        setOn(color)
+    }
+    return (
+        <div className="container">
+            <div className="pipe"></div>
+            <div className="box-light">
+
+                <div className={`red ${on == "red" ? "light-on" : ""}` } 
+                     onClick={()=> changeColor("red")}   
+                ></div>
+                <div className={`yellow ${on == "yellow" ? "light-on" : ""}`}
+                     onClick={()=> changeColor("yellow")}
+                ></div>
+                <div className={`green ${on == "green" ? "light-on" : ""}`}
+                     onClick={()=> changeColor("green")}
+                ></div>
+
+            </div>
+        </div>
+    )
+}
+
+export default TrafficLigt
